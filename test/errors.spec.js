ed82905fbfd214cc80ff4af36aa23892f4973c1a
Yuri Gor <yuri.horus@gmail.com>
2019-05-09 02:39:40 +0300

    test: refactoring

--- test/errors.spec.js
+++ test/errors.spec.js
@@ -3,20 +3,21 @@ const errors = require('./../src/errors');
 
 const errorNames = Object.keys(errors);
 
+const validateError = (error, expectedMessage) => {
+  expect(error).to.have.property('message').and.equal(expectedMessage);
+  expect(JSON.stringify(error)).to.equal(`{"message":"${expectedMessage}"}`);
+};
+
 describe('Testing Chester Errors', () => {
   it('Default Constructor', () => {
     errorNames.forEach((errorName) => {
-      const error = new errors[errorName]();
-      expect(error).to.have.property('message').and.equal(errorName);
-      expect(JSON.stringify(error)).to.equal(`{"message":"${errorName}"}`);
+      validateError(new errors[errorName](), errorName);
     });
   });
 
   it('Message Passed to Constructor', () => {
     errorNames.forEach((errorName) => {
-      const error = new errors[errorName]("It's by design!");
-      expect(error).to.have.property('message').and.equal("It's by design!");
-      expect(JSON.stringify(error)).to.equal('{"message":"It\'s by design!"}');
+      validateError(new errors[errorName]("It's by design!"), "It's by design!");
     });
   });
 });
